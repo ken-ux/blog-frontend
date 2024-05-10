c30dfa14e2473a2be506e624d6ab8e5f43e8996b
Kenny Nguyen <sokenns@gmail.com>
2024-05-10 12:27:39 -0700

    Add Posts type

--- src/types.ts
+++ src/types.ts
@@ -1,7 +1,13 @@
-export interface PostProps {
+export interface Post {
   title: string;
   text: string;
   timestamp: string;
   published?: boolean;
   _id?: string;
+}
+
+export interface Posts {
+  isLoading: boolean;
+  posts: Array<Post>;
+  setPosts: React.Dispatch<React.SetStateAction<Post[]>>;
 }
