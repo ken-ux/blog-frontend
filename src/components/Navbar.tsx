64c90aa2768413c3762fad6e9086119fb8f05a8c
Kenny Nguyen <sokenns@gmail.com>
2024-05-10 14:36:58 -0700

    Style Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,21 @@ import { Navbar as NavbarProps } from "../types";
 import { Post as PostProps } from "../types";
 
 function Navbar({ posts }: NavbarProps) {
+  const postList = posts.map((post: PostProps) => {
+    return (
+      <li
+        key={post._id}
+        className="list-disc transition-all hover:text-slate-50/60"
+      >
+        <a href={"#" + post._id}>{post.title}</a>
+      </li>
+    );
+  });
+
   return (
-    <nav className="col-span-2 col-start-7 row-start-1 p-8">
-      <h1 className="text-xl">Navigation</h1>
-      <ul>
-        {posts.map((post: PostProps) => {
-          return (
-            <li key={post._id}>
-              <a href={"#" + post._id}>{post.title}</a>
-            </li>
-          );
-        })}
-      </ul>
+    <nav className="col-span-2 col-start-7 row-start-1 flex flex-col gap-2 rounded-lg border p-8 text-slate-50">
+      <h1 className="text-xl">Jump to Post</h1>
+      <ul className="list-inside">{postList}</ul>
     </nav>
   );
 }
