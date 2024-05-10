95d24a951ea3efd9979894c01f1db6961185119d
Kenny Nguyen <sokenns@gmail.com>
2024-05-10 12:46:21 -0700

    Add Navbar type

--- src/types.ts
+++ src/types.ts
@@ -2,12 +2,17 @@ export interface Post {
   title: string;
   text: string;
   timestamp: string;
-  published?: boolean;
+  id: string | undefined;
   _id?: string;
+  published?: boolean;
 }
 
 export interface Posts {
   isLoading: boolean;
   posts: Array<Post>;
   setPosts: React.Dispatch<React.SetStateAction<Post[]>>;
+}
+
+export interface Navbar {
+  posts: Array<Post>;
 }
