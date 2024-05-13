f63a8ede728f76ed4b31b77724346d401df6a17e
Kenny Nguyen <sokenns@gmail.com>
2024-05-13 00:44:55 -0700

    Show comment count on posts

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,15 +1,31 @@
 import { Post as PostProps } from "../types";
+import { useEffect } from "react";
+import { useState } from "react";
+const apiUrl: string = import.meta.env.VITE_API_URL;
 
 function Post({ title, text, timestamp, id }: PostProps) {
+  const [commentCount, setCommentCount] = useState(0);
   const date = new Date(timestamp).toLocaleDateString();
+
+  useEffect(() => {
+    async function getPosts() {
+      const response = await fetch(`${apiUrl}/posts/${id}/comments`);
+      const data = await response.json();
+      setCommentCount(data.length);
+    }
+    getPosts();
+  });
 
   return (
     <div className="rounded-lg border bg-white shadow-md" id={id}>
       <span className="post-header flex items-center gap-2 rounded-t-lg p-4 text-white">
-        <h3 className="text-lg font-semibold">{title}</h3>
+        <h3 className="text-lg font-medium">{title}</h3>
         <p className="text-sm italic">(Posted: {date})</p>
       </span>
-      <p className="p-4">{text}</p>
+      <div className="p-4">
+        <p>{text}</p>
+        <p>Comments: {commentCount}</p>
+      </div>
     </div>
   );
 }
